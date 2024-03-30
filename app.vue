63a34a0a309d0fb76e4fbde8e4c42d7373bd8ab9
Noah Horlacher <kuromarumusic@gmail.com>
2024-03-30 13:19:45 +0100

    Create app.vue

---
+++ app.vue
@@ -0,0 +1,24 @@
+<script setup>
+useHead({
+  title: 'SatAi',
+  bodyAttrs: {
+    class: 'bg-gray-200'
+  },
+  meta: [
+    { name: 'description', content: 'A midi generation tool based on SatAi GAN' },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+  ]
+})
+</script>
+
+<template>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
+
+<style>
+html {
+  font-family: 'Poppins', sans-serif;
+}
+</style>
