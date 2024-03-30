ea8939467d2ce3616efbef036d67e254e91551db
Noah Horlacher <kuromarumusic@gmail.com>
2024-03-30 13:20:33 +0100

    Create app-section.vue

---
+++ components/app-section.vue
@@ -0,0 +1,13 @@
+<script setup>
+const props = defineProps({
+    title: String,
+
+})
+</script>
+
+<template>
+    <section class="mx-auto w-full">
+        <h2 v-if="title" v-once class="text-2xl mb-6">{{ title }}</h2>
+        <slot />
+    </section>
+</template>
