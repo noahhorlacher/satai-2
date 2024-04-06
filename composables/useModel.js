7b8e2e614ff2d142d1dcb7910d5a6dc2296f2bcd
Noah Horlacher <kuromarumusic@gmail.com>
2024-04-06 10:46:17 +0200

    moved model from store to composable

---
+++ composables/useModel.js
@@ -0,0 +1,27 @@
+export function useModel(settings = {
+    outputDimensions: {
+        x: 128,
+        y: 64
+    },
+    discriminator: {
+        learningRate: 0.0002,
+        clipValue: 0.01
+    },
+    generator: {
+        amountInputParameters: 100
+    },
+    GAN: {
+        learningRate: 0.0002,
+        clipValue: 0.01
+    }
+}) {
+    const discriminator = createDiscriminatorModel(settings.outputDimensions, settings.discriminator.learningRate, settings.discriminator.clipValue)
+    const generator = createGeneratorModel(settings.outputDimensions, settings.generator.amountInputParameters)
+    const gan = createGANModel(generator, discriminator, settings.GAN.learningRate, settings.GAN.clipValue)
+
+    return {
+        discriminator,
+        generator,
+        gan
+    }
+}
